b856f69a73eb07eac3d6f532168aa3b5b2215a7d
alexholmes99 <alexh@chillibean.com>
2023-03-04 21:01:28 +0000

    fighter name & divsion

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -10,10 +10,17 @@
 #  updated_at :datetime         not null
 #
 class Fighter < ApplicationRecord
+  enum division: {fly_weight: 0, bantam_weight: 1, feather_weight: 2, light_weight: 3, welter_weight: 4, middle_weight: 5, light_heavy_weight: 6, heavy_weight: 7}
+  
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :division, presence: true
+  
+  def full_name
+    return first_name + ' ' + last_name
+  end
 
-
-  enum division: {fly_weight: 0, bantam_weight: 1, feather_weight: 2, light_weight: 3, welter_weight: 4, middle_weight: 5, light_heavy_weight: 6, heavy_weight: 7}
+  def division_string
+    return division.gsub('_', '').titleize
+  end
 end
