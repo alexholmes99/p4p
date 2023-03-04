4d061fe04cb6c6101252ad3dc7d727983709711d
alexholmes99 <alexh@chillibean.com>
2023-03-04 20:27:52 +0000

    enum division

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -10,5 +10,5 @@
 #  updated_at :datetime         not null
 #
 class Fighter < ApplicationRecord
-  
+  enum divison: {fly_weight: 0, bantam_weight: 1, feather_weight: 2, light_weight: 3, welter_weight: 4, middle_weight: 5, light_heavy_weight: 6, heavy_weight: 7}
 end
