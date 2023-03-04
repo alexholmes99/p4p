25254386941ee0db7f3d47eb2526ef9122a75296
alexholmes99 <alexh@chillibean.com>
2023-03-04 20:34:19 +0000

    validation

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -10,5 +10,10 @@
 #  updated_at :datetime         not null
 #
 class Fighter < ApplicationRecord
-  enum divison: {fly_weight: 0, bantam_weight: 1, feather_weight: 2, light_weight: 3, welter_weight: 4, middle_weight: 5, light_heavy_weight: 6, heavy_weight: 7}
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :division, presence: true
+
+
+  enum division: {fly_weight: 0, bantam_weight: 1, feather_weight: 2, light_weight: 3, welter_weight: 4, middle_weight: 5, light_heavy_weight: 6, heavy_weight: 7}
 end
