d4cac4aeb966976b26beff73cf0410cd93dc2214
alexholmes99 <alexh@chillibean.com>
2023-03-06 12:18:08 +0000

    fixed predefined stylings

--- app/javascript/components/Index.js
+++ app/javascript/components/Index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Logo from "./Logo";
 import RankContainer from "./RankContainer";
+import styled from "styled-components";
 
 function Index() {
   return (
-    <>
+    <Wrapper>
       <Logo titleText="P4P" />
       <RankContainer />
-    </>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  background: black;
+  min-height: 100vh;
+`;
 
 export default Index;
