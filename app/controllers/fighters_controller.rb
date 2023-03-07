032b3501aacc16e6b58dfbefb625e9360e748048
alexholmes99 <alexh@chillibean.com>
2023-03-07 18:02:28 +0000

    extracting necessary fighter info

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -1,5 +1,10 @@
 class FightersController < ApplicationController
   def index
-    @fighters = Fighter.all
+   
+    @fighters = Fighter.all.map {|fighter| {
+      "fullname": fighter.full_name,
+      "division": fighter.division_string,
+      "avatar": url_for(Fighter.first.avatar)   
+    }}
   end
 end
